2343a3e0ce3918c9db0cfa38cc686f63fa63ca90
mig7999 <mig7999@gmail.com>
2015-12-31 15:59:40 -0600

    Update WW_MANUAL.java
    Added attempt to close app gracefully

--- WW_MANUAL.java
+++ WW_MANUAL.java
@@ -17,6 +17,8 @@ import org.swerverobotics.library.interfaces.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import static org.swerverobotics.library.internal.Util.handleCapturedInterrupt;
+
 /**
  * An example of a synchronous opmode that implements a simple drive-a-bot. 
  */
@@ -70,6 +72,7 @@ public class WW_MANUAL extends SynchronousOpMode
         double position_s3 = 0.5;
         double position_s4 = 0.5;
         double position_s5 = 0.5;
+        boolean KeepRunning = true;
 
 
 
@@ -81,19 +84,29 @@ public class WW_MANUAL extends SynchronousOpMode
         // step you did in the FTC Robot Controller app on the phone.
            // devMode = DcMotorController.DeviceMode.WRITE_ONLY;
 
+/*
         frontright = hardwareMap.dcMotor.get("frontright");
         //    frontright =    (DcMotor)HardwareUtil.getHardwareOn("frontright",  hardwareMap.dcMotor);
         backright = hardwareMap.dcMotor.get("backright");
 
         frontleft = hardwareMap.dcMotor.get("frontleft");
         backleft = hardwareMap.dcMotor.get("backleft");
+*/
+            frontright =    (DcMotor)HardwareUtil.getHardwareOn("frontright",  hardwareMap.dcMotor);
+            backright =    (DcMotor)HardwareUtil.getHardwareOn("backright",  hardwareMap.dcMotor);
+            frontleft =    (DcMotor)HardwareUtil.getHardwareOn("frontleft",  hardwareMap.dcMotor);
+            backleft =    (DcMotor)HardwareUtil.getHardwareOn("backleft",  hardwareMap.dcMotor);
 
-
-            s1 = hardwareMap.servo.get("servo1");
+/*            s1 = hardwareMap.servo.get("servo1");
             s2 = hardwareMap.servo.get("servo2");
             s3 = hardwareMap.servo.get("servo3");
             s4 = hardwareMap.servo.get("servo4");
-            s5 = hardwareMap.servo.get("servo5");
+            s5 = hardwareMap.servo.get("servo5");*/
+            s1 = (Servo)HardwareUtil.getHardwareOn("servo1",  hardwareMap.servo);
+            s2 = (Servo)HardwareUtil.getHardwareOn("servo2",  hardwareMap.servo);
+            s3 = (Servo)HardwareUtil.getHardwareOn("servo3",  hardwareMap.servo);
+            s4 = (Servo)HardwareUtil.getHardwareOn("servo4",  hardwareMap.servo);
+            s5 = (Servo)HardwareUtil.getHardwareOn("servo5",  hardwareMap.servo);
 
             double s1_MIN_MAX_LIMITS[] = {0.0,1.0};
             double s2_MIN_MAX_LIMITS[] = {0.1,0.99};
@@ -111,21 +124,34 @@ public class WW_MANUAL extends SynchronousOpMode
 
 
 
+
         parameters.angleunit      = IBNO055IMU.ANGLEUNIT.DEGREES;
         parameters.accelunit      = IBNO055IMU.ACCELUNIT.METERS_PERSEC_PERSEC;
         parameters.loggingEnabled = false;
         parameters.loggingTag     = ",BNO055";
-        imu = ClassFactory.createAdaFruitBNO055IMU(hardwareMap.i2cDevice.get("imu"), parameters);
-            waitOneFullHardwareCycle();
-            v_sensor_ods = hardwareMap.opticalDistanceSensor.get ("sensor_ods");
-        // Configure the dashboard however we want it
-            this.configureDashboard();
-        
+
+            try
+            {
+                imu = ClassFactory.createAdaFruitBNO055IMU(hardwareMap.i2cDevice.get("imu"), parameters);
+                //    imu = (IBNO055IMU)HardwareUtil.getHardwareOn("imu",  hardwareMap.i2cDevice);
+                //v_sensor_ods = hardwareMap.opticalDistanceSensor.get ("sensor_ods");
+                this.configureDashboard();
+            }
+            catch (Exception e)
+            {
+                KeepRunning = false;
+                //System.exit(0);
+            }
+
+        v_sensor_ods = (OpticalDistanceSensor)HardwareUtil.getHardwareOn("sensor_ods",  hardwareMap.opticalDistanceSensor);
         // Wait until we've been given the ok to go
         waitForStart();
-        
-        // Enter a loop processing all the input we receive
-        while (opModeIsActive())
+
+            // Configure the dashboard however we want it
+
+
+            // Enter a loop processing all the input we receive
+        while ((opModeIsActive())&& (KeepRunning))
             {
             // Emit telemetry with the freshest possible values
             telemetry.update();
@@ -206,9 +232,11 @@ public class WW_MANUAL extends SynchronousOpMode
             //waitOneFullHardwareCycle();
 
             // Let the rest of the system run until there's a stimulus from the robot controller runtime.
-            this.idle();
+            if (KeepRunning) this.idle();
             }
         }
+
+
 
     /**
      * Implement a simple two-motor driving logic using the left and right
@@ -362,7 +390,7 @@ public class WW_MANUAL extends SynchronousOpMode
                             return formatDecimal(RearWheelsFlippingAvoidanceVal);
                         }
                     }));
-            this.telemetry.addLine
+/*            this.telemetry.addLine
                     (
                             this.telemetry.item("s1:", new IFunc<Object>() {
                                 @Override
@@ -385,7 +413,7 @@ public class WW_MANUAL extends SynchronousOpMode
                                     return formatDecimal(s4.getPosition());
                                 }
                             })
-                    );
+                    );*/
             this.telemetry.addLine
                     (
                             this.telemetry.item("joystick:", new IFunc<Object>() {
@@ -481,11 +509,21 @@ public class WW_MANUAL extends SynchronousOpMode
 
         void SetServoPositions()
         {
-            s1.setPosition(position_s1);
-            s2.setPosition(position_s2);
-            s3.setPosition(position_s3);
-            s4.setPosition(position_s4);
-            s5.setPosition(position_s5);
+            try
+            {
+                s1.setPosition(position_s1);
+                s2.setPosition(position_s2);
+                s3.setPosition(position_s3);
+                s4.setPosition(position_s4);
+                s5.setPosition(position_s5);
+            }
+            catch (Exception e)
+            {
+                KeepRunning = false;
+                //System.exit(0);
+            }
+
+
         }
 
         double Update_Stick_Position_w_limit(double stick_value, double position_value, double min_Limit, double max_limit )
